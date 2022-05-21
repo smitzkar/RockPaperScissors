b6d5d7aab937a4736d41aa959758af110e7f6404
smitzkar <karl.e.schmitz@hotmail.com>
2022-05-21 13:17:07 +0200

    Add computer choice function

--- script.js
+++ script.js
@@ -5,21 +5,42 @@ function scriptTest() {
 }
 
 // Take player choice and validate
-function playerInput() {
+function playerAnswer() {
 
   const possibleInput = ["rock", "paper", "scissors"];
+  let playerHand; 
   let validated = false;
 
+  // Loop asking player for input until valid answer
   while (validated == false) {
-    let tempVar = prompt("Please choose from rock, paper or scissors.");
-    if (possibleInput.includes(tempVar)) {
+    playerHand = prompt("Please choose from rock, paper or scissors.");
+    if (possibleInput.includes(playerHand)) {
       validated = true;
     }
   }
+  return playerHand;
 }
+
+// Return randomInt from 0-max
+function getRandInt(max) {
+  return Math.floor(Math.random()*max);
+}
+
+// Generate computer choice -> rng
+function computerAnswer() {
+  return getRandInt(3);
+
+}
+
+// Compare player vs computer choice, return answer -> case
+
+// Return winner -> using case variable from above
+
 
 // Main function to start the game
 function gameStart(){
-  let playerChoice = playerInput();
+  let playerChoice = playerAnswer();
+  console.log(`You chose: `, playerChoice)
   console.log("Computer is working...");
 }
+
