812ce1b0f00c8091034eec5385775a3a53aa0446
smitzkar <karl.e.schmitz@hotmail.com>
2022-05-21 12:40:34 +0200

    Add Input function

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 // Rock Paper Scissors
 
-function scriptTest (){
+function scriptTest() {
   console.log("Script loaded.");
+}
+
+function playerInput() {
+  console.log("Choose between rock/paper/scissors.\n Alternatively enter 1, 2, 3.");
+  let tempVar = prompt("Rock, paper or scissors?");
+  const possibleInput = ["rock", "paper", "scissors"];
+  if (!possibleInput.includes(tempVar)) {
+    console.log("Please enter 'rock', 'paper' or 'scissors'!");
+  }
+}
+
+function gameStart(){
+  let playerChoice = playerInput();
 }
